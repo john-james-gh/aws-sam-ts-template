9a9e1d3e6423a76b4e33b3bdc6b6818be58698b1
James <john.usa.james@gmail.com>
2025-03-25 22:49:14 +0000

    add pino logger and update get-all handler; create tsconfig for tests

--- src/get-all.ts
+++ src/get-all.ts
@@ -1,14 +1,28 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import pino from "pino"
+
+const logger = pino({
+  // Disable the default logging properties (pid, hostname), which overlap with AWS CloudWatch
+  base: null,
+  // Disable the timestamp in log messages, which overlap with AWS CloudWatch
+  timestamp: false,
+  formatters: {
+    level(label) {
+      // Return the log level named as e.g. 'info' rather than '30'
+      return { level: label }
+    },
+  },
+})
 
 export const handler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
-  console.info("event", event)
+  logger.info({ event }, "Event")
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "hello everyone",
+      message: "hello everyone!",
     }),
   }
 }
