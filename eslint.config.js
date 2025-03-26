ab1bb3b5f17a3cca333502df266ce5fbca1b22fa
James <john.usa.james@gmail.com>
2025-03-26 15:23:46 +0000

    feat: update ESLint configuration to use globalIgnores for improved file exclusion

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "eslint/config"
+import { defineConfig, globalIgnores } from "eslint/config"
 import globals from "globals"
 import js from "@eslint/js"
 import tseslint from "typescript-eslint"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 
 export default defineConfig([
-  { ignores: ["dist", ".aws-sam", "coverage"] },
+  globalIgnores([".aws-sam", "coverage"]),
   {
     files: ["src/**/*.{js,mjs,cjs,ts}"],
     languageOptions: { globals: globals.node },
